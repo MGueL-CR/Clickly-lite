5c81a008332c208eb375bcdf7e3806a2dda04b25
Miguel M. <91512302+MGueL-CR@users.noreply.github.com>
2025-03-22 14:44:10 -0600

    Version v1.3
    Version v1.3:
    
    -  Uso del https al redirigir a la pagina Inicio de Vortex

--- ext/src/js/main.js
+++ ext/src/js/main.js
@@ -2,14 +2,15 @@ try {
     window.addEventListener("load", () => {
         try {
             if (validarContenidoURL("crvle-vporequests")) { mainQueueIC(); return; }
-    
+
             if (validarContenidoURL("RunCardFilter")) { mainFormRC(); return; }
-    
+
             if (validarContenidoURL("RunCard.aspx")) { mainPrintRC(); return; }
-    
-            if (validarContenidoURL("Error.aspx")) { 
-                abrirNuevoEnlace('http://vortexreports.intel.com/Index.aspx', '_self');
-                return; }
+
+            if (validarContenidoURL("Error.aspx")) {
+                abrirNuevoEnlace('https://vortexreports.intel.com/Index.aspx', '_self');
+                return;
+            }
         } catch (err) {
             mostrarAlertaError(err);
         }
