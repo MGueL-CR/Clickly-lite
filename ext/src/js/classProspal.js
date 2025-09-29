c4f32af8617ee86a54a2238035a3e82c55d917b4
MGueL-Dev <mguel.dev@proton.me>
2025-09-29 13:32:55 -0600

    Crear Clase Prospal

---
+++ ext/src/js/classProspal.js
@@ -0,0 +1,12 @@
+class ClassProspal {
+    owner = "ENG";
+    fabID = "D1D";
+    assyID = "A11";
+    materialCode = "M"
+
+    constructor(pMRS, pWWID, pITEMS) {
+        this.numMRS = pMRS;
+        this.userID = pWWID;
+        this.listItems = pITEMS;
+    }
+}
