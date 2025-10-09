3185418ebbe8ec97d0e7472902e864d405ac850a
MGueL-Dev <mguel.dev@proton.me>
2025-10-09 16:08:38 -0600

    Completar Formulario Prospal II

--- ext/src/js/pgProspalLMT.js
+++ ext/src/js/pgProspalLMT.js
@@ -7,8 +7,7 @@ function mainProspalLMT() {
                 console.clear();
                 generarClaseProspal();
                 completarFormSuperior();
-                agregarBotonContinuar();
-            }, 2000);
+            }, 3000);
         }
     } catch (error) {
         mostrarAlertaError(error);
@@ -16,13 +15,13 @@ function mainProspalLMT() {
 }
 
 function agregarBotonContinuar() {
-    const filaBotones = obtenerFilas().item(13);
+    const filaBotones = obtenerFilasFormulario().item(13);
     const divBotones = obtenerElementosPorTags(filaBotones, 'div').item(0).firstElementChild;
     const cantidadItems = vObj.prospal.obtenerListaMaterial().length;
 
-    const btnContinuar = nuevoBoton('btnContinuar', 'btn btn-info', 'Completar Campos');
-    const spnIcon = nuevoSpan('spnIcono', 'intelicon-new', '');
-    const spnCount = nuevoSpan('spnCounter', '', `Continuar (0/${cantidadItems})`);
+    const btnContinuar = nuevoBoton('btnContinuar', 'btn btn-info mx-1', 'Completar Campos');
+    const spnIcon = nuevoSpan('spnIcono', 'intelicon-play', '');
+    const spnCount = nuevoSpan('spnCounter', 'px-1', `Continuar (0/${cantidadItems})`);
 
     btnContinuar.addEventListener('click', completarFormInferior);
 
@@ -32,12 +31,30 @@ function agregarBotonContinuar() {
 
 function generarClaseProspal() {
     const urlParams = obtenerParametrosURL();
-    vObj.prospal = urlParams.size > 0 ? new ClassProspal(urlParams.get("MRS"), urlParams.get("WWID"), urlParams.get("ITEMS")) : null;
+    vObj.prospal = urlParams.size > 0 ?
+        new ClassProspal(
+            urlParams.get("MRS"),
+            urlParams.get("WWID"),
+            urlParams.get("ITEMS")) :
+        null;
 }
 
-function obtenerFilas() {
+function obtenerFilasFormulario() {
     const tabla = document.getElementsByClassName('manualTravelcreatelayoutTable').item(0);
     return tabla.rows;
+}
+
+function obtenerCampoTexto(pFila, pIndex) {
+    return obtenerElementosPorTags(pFila, 'input').item(pIndex);
+}
+
+function obtenerCampoLista(pFila, pIndex) {
+    return obtenerElementosPorTags(pFila, 'select').item(pIndex);
+}
+
+function agregarValorAlCampo(pInput, pValor) {
+    pInput.focus();
+    pInput.value = pValor;
 }
 
 function completarFormSuperior() {
@@ -46,16 +63,31 @@ function completarFormSuperior() {
     pestañas.lastElementChild.click();
 
     setTimeout(() => {
-        const filas = obtenerFilas();
-        const inputMaterialCode = obtenerElementosPorTags(filas.item(2), 'input').item(1);
-        inputMaterialCode.value = vObj.prospal.materialCode;
-        const inputWWID = obtenerElementosPorTags(filas.item(3), 'input').item(0);
-        inputWWID.focus();
-        inputWWID.value = vObj.prospal.userID;
-        obtenerElementosPorTags(filas.item(3), 'input').item(1).focus();
+        agregarBotonContinuar();
+        const filas = obtenerFilasFormulario();
+        const txtMaterialCode = obtenerCampoTexto(filas.item(2), 1);
+        const txtWWID = obtenerCampoTexto(filas.item(3), 0);
+        agregarValorAlCampo(txtMaterialCode, vObj.prospal.materialCode);
+        agregarValorAlCampo(txtWWID, vObj.prospal.userID);
+        obtenerCampoTexto(filas.item(3), 1).focus();
     }, 2000);
 }
 
+function obtenerCamposFormulario() {
+    const filas = obtenerFilasFormulario();
+    return {
+        "cmbPartType": obtenerCampoTexto(filas.item(14), 0),
+        "txtLot": obtenerCampoTexto(filas.item(14), 1),
+        "cmbOwner": obtenerCampoLista(filas.item(16), 0),
+        "cmbSiteId": obtenerCampoLista(filas.item(17), 0),
+        "cmbAssyId": obtenerCampoLista(filas.item(18), 0),
+        "txtQty": obtenerCampoTexto(filas.item(19), 0),
+        "txtCommentMRS": obtenerElementosPorTags(filas.item(19), 'textarea').item(0)
+    }
+}
+
 function completarFormInferior() {
-    //const objProspal = generarClaseProspal();
+    const objProspal = vObj.prospal;
+    const listaItems = objProspal.obtenerListaMaterial();
+    const form = obtenerCamposFormulario();
 }
