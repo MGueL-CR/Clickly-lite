da081f1ce35268fa588fa6d3cafdb767aae023b7
MGueL-CR <mi.migue437@gmail.com>
2025-10-29 22:02:12 -0600

    Refactorizar BP.js

--- ext/src/js/batchProcessor.js
+++ ext/src/js/batchProcessor.js
@@ -1,14 +1,14 @@
 function mostrarListaLotes(pSources) {
     const listaLotes = pSources.map(item => `\t${item.trim().split(' ').at(0)} ✔\n`);
-    return `\n\nLots:\n${listaLotes.join('')}`;
+    return `\n\nLots:\n${listaLotes.sort().join('')}`;
 }
 
 function mostrarLotes(pContenido) {
     const numIni = pContenido.includes('|') ? pContenido.indexOf('|') : pContenido.indexOf(':');
-    const substring1 = pContenido.substring(numIni + 1);
-    const numFin = substring1.indexOf('(') >= 1 ? substring1.indexOf('(') : substring1.length;
-    const substring2 = substring1.substring(0, numFin).split(',').sort();
-    return mostrarListaLotes(substring2);
+    const subString1 = pContenido.substring(numIni + 1);
+    const numFin = subString1.includes('(') ? subString1.indexOf('(') : subString1.length;
+    const subString2 = subString1.substring(0, numFin).split(',');
+    return mostrarListaLotes(subString2);
 }
 
 function formatoLista(pArreglo) {
